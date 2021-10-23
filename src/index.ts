c458165cd173198d6ce2fb8a9606180a0b56e3d0
Petr Huřťák <petr.hurtak@gmail.com>
2021-10-23 14:49:42 +0200

    add express

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,27 @@
 import sqlite3 from "sqlite3";
 import { open } from "sqlite";
+import express from "express";
 
 const main = async (): Promise<void> => {
-	const db = await open<sqlite3.Database, sqlite3.Statement>({
+	const db = await open({
 		filename: ":memory:",
 		driver: sqlite3.cached.Database,
 	});
-	// cons
 	await db.exec(`CREATE TABLE tbl (col TEXT)`);
 	await db.exec(`INSERT INTO tbl VALUES ("test")`);
 
 	const result = await db.get(`SELECT col FROM tbl WHERE col = ?`, "test");
 	console.log(result);
-	console.log(2);
-	console.log(3);
+
+	const server = express();
+
+	server.get("/", function (req, res) {
+		res.json({ hello: 1 });
+	});
+
+	server.listen(3000, () => {
+		console.log(`Server running at http://localhost:3000`);
+	});
 };
 
 main();
