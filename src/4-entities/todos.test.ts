8d8692defdaa2c93481af0892a169eb8d1166c4c
Petr Hurtak <petr.hurtak@gmail.com>
2022-02-06 23:37:22 +0100

    add tests

---
+++ src/4-entities/todos.test.ts
@@ -0,0 +1,31 @@
+import { createTodo, Todo, TodoTextTooLong, TodoTextTooShort, validateTodoProperties } from "./todos";
+
+describe("todos", () => {
+	const t: Todo = { id: 1, text: "text", completed: false };
+
+	describe("createTodo", () => {
+		test("creates todo", () => {
+			expect(createTodo(t.id, t.text, t.completed)).toEqual(t);
+		});
+		test("returns error on too short text", () => {
+			expect(createTodo(t.id, "", t.completed)).toBeInstanceOf(TodoTextTooShort);
+		});
+		test("returns error on too long text", () => {
+			expect(createTodo(t.id, "x".repeat(1000), t.completed)).toBeInstanceOf(TodoTextTooLong);
+		});
+	});
+
+	describe("validateTodoProperties", () => {
+		test("returns the same object when valid", () => {
+			expect(validateTodoProperties(t)).toEqual(t);
+			expect(validateTodoProperties({ text: "text" })).toEqual({ text: "text" });
+			expect(validateTodoProperties({})).toEqual({});
+		});
+		test("returns error on too short text", () => {
+			expect(validateTodoProperties({ text: "" })).toBeInstanceOf(TodoTextTooShort);
+		});
+		test("returns error on too long text", () => {
+			expect(validateTodoProperties({ text: "x".repeat(1000) })).toBeInstanceOf(TodoTextTooLong);
+		});
+	});
+});
