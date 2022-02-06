a6b6e44d526014dda75ebc2b8530aeb4b8d73093
Petr Hurtak <petr.hurtak@gmail.com>
2022-02-06 22:12:26 +0100

    test test helpers

---
+++ src/utils/jest.test.ts
@@ -0,0 +1,23 @@
+import { getId, idDoesNotExist } from "./jest";
+
+describe("test helpers", () => {
+	describe("getId", () => {
+		const iterations = 100;
+		test("all ids are different", () => {
+			const ids = new Set<number>();
+			for (let i = 0; i < iterations; i++) {
+				const id = getId();
+				expect(ids.has(id)).toBe(false);
+				ids.add(id);
+			}
+		});
+		test("all ids are not idDoesNotExist", () => {
+			const ids = new Set<number>();
+			for (let i = 0; i < iterations; i++) {
+				const id = getId();
+				ids.add(id);
+			}
+			expect(ids.has(idDoesNotExist)).toBe(false);
+		});
+	});
+});
